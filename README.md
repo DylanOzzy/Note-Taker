4a87318154b66f6aa369d3a56a5401d23d89b611
unknown <dylanosborn123@hotmail.com>
2023-12-05 23:33:34 -0600

    Added server side code that allows notes to be displayed in the left hand column. Added README.md content.

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# Note-Taker
+# Dylan's Note-Taker Application
+
+## Table of Contents
+
+- [Project Overview](#project-overview)
+- [Built With](#built-with)
+- [Features](#features)
+- [Screenshots/Deploy](#screenshotsdeploy)
+- [License](#license)
+
+
+## Project Overview
+
+I made this webpage to demonstrate my current level of knowledge with Javascript and to showcase my ability to build light applications. 
+
+## Built With
+
+- HTML
+- CSS
+- Javascript
+- NodeJS and Node Package Manager
+- Express
+- Heroku
+
+## Features
+
+- User is presented with a landing page with a link to a notes page.
+- User is presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+-  Accessible "Save Note" button and a "Clear Form" button appear in the navigation at the top of the page.
+- When user clicks on the Save button, the new note user has entered is saved and appears in the left-hand column with the other existing notes.
+
+## Screenshots/Deploy
+
+ [Note-Taker Application](https://github.com/DylanOzzy/Note-Taker)
+
+ ![Note-Taker Application](assets/NoteTakerSS.png)
+
+ [Heroku Link]()
+
+
+
+
+## License
+
+MIT License
+
+Copyright (c) [2023] Dylan Osborn
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- server.js
+++ server.js
@@ -1,46 +1,64 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs').promises;
+const { readFromFile, writeToFile, readAndAppend } = require('./public/assets/helpers/fsUtils');
+
 const app = express();
 const PORT = 3001;
-const fs = require('fs');
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'));
-});
+app.get('/api/notes', async (req, res) => {
+  try {
+      const data = await readFromFile('./db/db.json');
 
-app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request received to add a note`);
-  const { title, text } = req.body;
-  if (title && text ) {
-    const newNote = {
-      title,
-      text
-    };
-    
-    const response = {
-      body: newNote,
-    };
-
-    res.status(201).json(response);
-    fs.appendFile(__dirname + '/db/db.json', JSON.stringify(response, null, 2) + '\n', (err) => {
-      if (err) {
-        console.error('Error writing to file:', err);
-      } else {
-        console.log('Data has been written to the file');
+      if (!data) {
+          res.json([]);
+          return;
       }
-    });
-  } else {
-    res.status(500).json('Error in saving note');
+
+      res.json(JSON.parse(data));
+  } catch (error) {
+      console.error('Error reading notes:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.post('/api/notes', async (req, res) => {
+    try {
+        console.info(`${req.method} request received to add a note`);
+        const { title, text } = req.body;
+
+        if (title && text) {
+            const newNote = { title, text };
+
+            const filePath = path.join(__dirname, '/db/db.json');
+            const existingNotes = JSON.parse(await fs.readFile(filePath, 'utf-8') || '[]');
+
+            existingNotes.push(newNote);
+
+            await fs.writeFile(filePath, JSON.stringify(existingNotes, null, 2));
+
+            res.status(201).json({ body: newNote });
+            console.log('Data has been written to the file');
+        } else {
+            res.status(400).json({ error: 'Invalid request. Title and text are required.' });
+        }
+    } catch (error) {
+        console.error('Error adding note:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
